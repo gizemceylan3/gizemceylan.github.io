35f950892714cab538e47e163302a5f0ec2edee7
Gizem Ceylan <gceylan@gmail.com>
2025-03-30 15:17:51 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* script.js 
+   Author:
+   Date:
+*/
+
+$(document).ready(
+
+    function myFunction() {
+      var x = document.getElementById("myMenuitems");
+      if (x.className === "menuitems") {
+        x.className += " responsive";
+      } else {
+        x.className = "menuitems";
+      }
+    })
+
+
+
+
+
